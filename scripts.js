06c1ffdab45d2781b576c0c1fc7aa917bc58ecd2
Winter R <49598669+wrunion@users.noreply.github.com>
2019-09-20 13:23:56 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,82 @@
+//Business logic
+
+var suit;
+var cardNumber;
+var majorArcana = ['The Fool', 'The Magician', 'The High Priestess', 'The Empress', 'The Emperor', 'The Hierophant', 'The Lovers', 'The Chariot', 'Justice', 'The Hermit', 'Wheel of Fortune', 'Strength', 'The Hanged Man', 'Death', 'Temperance', 'The Devil', 'The Tower', 'The Star', 'The Moon', 'The Sun', 'Judgement', 'The World'];
+
+var getSuit = function() {
+    suit = Math.floor(Math.random()*6);
+
+    switch(suit) {
+        case 0:
+        case 1:
+            suit = "major arcana";
+            return suit;
+            break;
+        case 2:     
+        suit = "Wands";
+        return suit;
+        break;      
+        case 3:
+            suit = "Pentacles";
+            return suit;
+            break;   
+        case 4:
+            suit = "Swords";
+            return suit;
+            break; 
+        case 5: 
+            suit = "Cups";
+            return suit;
+            break;     
+        default: suit = "major arcana";
+        return suit;
+        break;  
+    }
+
+};
+
+var getCard = function() {
+    if (suit === "major arcana") {
+        cardNumber = Math.floor(Math.random() * 22);
+        return cardNumber;
+    } else {
+        cardNumber = Math.floor(Math.random() * 15);
+        switch(cardNumber) {
+            case 11:
+                cardNumber = "Page";
+                break;
+            case 12:
+                cardNumber = "Knight";
+                break;    
+            case 13: 
+                cardNumber = "Queen";
+                break;
+            case 14:
+                cardNumber = "King";
+                break;        
+        }
+        return cardNumber;
+    }
+}
+
+function displayCard() {
+if (suit === "major arcana") {
+    var card = majorArcana[cardNumber];
+    console.log(`Your card is ${card}. Thanks for playing!`);
+    alert(`Your card is ${card}. Thanks for playing!`);
+} else {
+    console.log(`Your card is ${cardNumber} of ${suit}. Thanks for playing!`);
+    alert(`Your card is ${cardNumber} of ${suit}. Thanks for playing!`);
+}
+
+}
+
+//UI logic
+
+//Once the user presses the button or submits the form, run these:
+getSuit();
+getCard();
+displayCard();
+
+
