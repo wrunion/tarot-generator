2688d61aa52f4be2a83c00ab2f56242ae00d1c05
Winter Runion <winterrunion@gmail.com>
2019-09-23 15:45:32 -0700

    Add swords cards to array.

--- scripts2.js
+++ scripts2.js
@@ -1,4 +1,10 @@
 //An array of all possible cards as objects
+
+//Order of suits/cards:
+// Major Arcana
+// Wands, Cups, Swords, Pentacles
+// Page Knight Queen King
+
 var cards = [
   {number: 0, name: "The Fool", url: "https://upload.wikimedia.org/wikipedia/en/9/90/RWS_Tarot_00_Fool.jpg"},
   {number: 1, name: 'The Magician', url: "https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg"},
@@ -35,11 +41,39 @@ var cards = [
   {number:22, name:'Page of Wands', url:"https://upload.wikimedia.org/wikipedia/en/6/6a/Wands11.jpg"},
   {number:23, name:'Knight of Wands', url:"https://upload.wikimedia.org/wikipedia/en/1/16/Wands12.jpg"},
   {number:24, name:'Queen of Wands', url:"https://upload.wikimedia.org/wikipedia/en/0/0d/Wands13.jpg"},
-  {number:25, name:'King of Wands', url:"https://upload.wikimedia.org/wikipedia/en/c/ce/Wands14.jpg"}
+  {number:25, name:'King of Wands', url:"https://upload.wikimedia.org/wikipedia/en/c/ce/Wands14.jpg"},
+  {number: 26, name: 'Ace of Cups', url:"https://upload.wikimedia.org/wikipedia/en/3/36/Cups01.jpg"},
+  {number: 26, name: 'Two of Cups', url:"https://upload.wikimedia.org/wikipedia/en/f/f8/Cups02.jpg"},
+  {number: 27, name: 'Three of Cups', url:"https://upload.wikimedia.org/wikipedia/en/7/7a/Cups03.jpg"},
+  {number: 28, name:'Four of Cups', url:"https://upload.wikimedia.org/wikipedia/en/3/35/Cups04.jpg"},
+  {number: 29, name: 'Five of Cups', url:"https://upload.wikimedia.org/wikipedia/en/d/d7/Cups05.jpg"},
+  {number: 30, name: 'Six of Cups', url:"https://upload.wikimedia.org/wikipedia/en/1/17/Cups06.jpg"},
+  {number: 31, name: 'Seven of Cups', url:"https://upload.wikimedia.org/wikipedia/en/a/ae/Cups07.jpg"},
+  {number: 32, name:'Eight of Cups', url:"https://upload.wikimedia.org/wikipedia/en/6/60/Cups08.jpg"},
+  {number: 33, name:'Nine of Cups', url:"https://upload.wikimedia.org/wikipedia/en/2/24/Cups09.jpg"},
+  {number: 34, name:'Ten of Cups', url:"https://upload.wikimedia.org/wikipedia/en/8/84/Cups10.jpg"},
+  {number: 35, name:'Page of Cups', url:"https://upload.wikimedia.org/wikipedia/en/a/ad/Cups11.jpg"},
+  {number: 36, name:'Knight of Cups', url:"https://upload.wikimedia.org/wikipedia/en/f/fa/Cups12.jpg"},
+  {number:37, name: 'Queen of Cups', url:"https://upload.wikimedia.org/wikipedia/en/6/62/Cups13.jpg"},
+  {number:38, name:'King Cups', url:"https://upload.wikimedia.org/wikipedia/en/0/04/Cups14.jpg"},
+  {number:39, name:'Ace of Swords', url:"https://upload.wikimedia.org/wikipedia/en/1/1a/Swords01.jpg"},
+  {number:40, name:'Two of Swords', url:"https://upload.wikimedia.org/wikipedia/en/9/9e/Swords02.jpg"},
+  {number:42, name:'Three of Swords', url:"https://upload.wikimedia.org/wikipedia/en/0/02/Swords03.jpg"},
+  {number:43, name:'Four of Swords', url:"https://upload.wikimedia.org/wikipedia/en/b/bf/Swords04.jpg"},
+  {number:44, name:'Five of Swords', url:"https://upload.wikimedia.org/wikipedia/en/2/23/Swords05.jpg"},
+  {number:45, name:'Six of Swords', url:"https://upload.wikimedia.org/wikipedia/en/2/29/Swords06.jpg"},
+  {number:46, name:'Seven of Swords', url:"https://upload.wikimedia.org/wikipedia/en/3/34/Swords07.jpg"},
+  {number:47, name:'Eight of Swords', url:"https://upload.wikimedia.org/wikipedia/en/a/a7/Swords08.jpg"},
+  {number: 48, name:'Nine of Swords', url:"https://upload.wikimedia.org/wikipedia/en/2/2f/Swords09.jpg"},
+  {number:49, name:'Ten of Swords', url:"https://upload.wikimedia.org/wikipedia/en/d/d4/Swords10.jpg"},
+  {number:50, name:'Page of Swords', url:"https://upload.wikimedia.org/wikipedia/en/4/4c/Swords11.jpg"},
+  {number:51, name:'Knight of Swords', url:"https://upload.wikimedia.org/wikipedia/en/b/b0/Swords12.jpg"},
+  {number:52, name:'Queen of Swords', url:"https://upload.wikimedia.org/wikipedia/en/d/d4/Swords13.jpg"},
+  {number:53, name:'King of Swords', url:"https://upload.wikimedia.org/wikipedia/en/3/33/Swords14.jpg"}
 ];
 
 var getCard = function() {
-    return Math.floor(Math.random()*26);
+    return Math.floor(Math.random()*54);
   };
 
 //Original draw functions, altared so as not to have repeat cards:
@@ -101,12 +135,5 @@ $(document).ready(function(){
 
 //$('#theDiv').prepend('<img id="theImg" src="theImg.png" />')
 
-
-
-//Order of suits/cards:
-// Tarot deck:
-// Major Arcana
-// Wands, Cups, Swords, Pentacles
-// Page Knight Queen King
 
 //Note: Can I do a hover/floating description box???
